9e50c73c81c8ed73054e4f9d5c07f57bb2739104
griffinpj <griffinpj@users.noreply.github.com>
2022-12-12 19:23:16 +0000

    Auto-Created Bundle

---
+++ static/wasm/wasm-bundle/target/release/deps/log-b10cccea715eab73.d
@@ -0,0 +1,9 @@
+/home/runner/work/blazingly_fast_portfolio/blazingly_fast_portfolio/static/wasm/wasm-bundle/target/release/deps/log-b10cccea715eab73.rmeta: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
+
+/home/runner/work/blazingly_fast_portfolio/blazingly_fast_portfolio/static/wasm/wasm-bundle/target/release/deps/liblog-b10cccea715eab73.rlib: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
+
+/home/runner/work/blazingly_fast_portfolio/blazingly_fast_portfolio/static/wasm/wasm-bundle/target/release/deps/log-b10cccea715eab73.d: /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs /home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs
+
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/lib.rs:
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/macros.rs:
+/home/runner/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/src/serde.rs:
